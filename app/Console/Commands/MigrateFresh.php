35258408cb0bf6238460aa187c1d4b30d794e41e
dafalagi <dafarizky34@gmail.com>
2025-12-27 19:06:01 +0700

    fix: correct command signature for migrate fresh command

--- app/Console/Commands/MigrateFresh.php
+++ app/Console/Commands/MigrateFresh.php
@@ -11,7 +11,7 @@ class MigrateFresh extends Command
      *
      * @var string
      */
-    protected $signature = 'app:migrate-fresh';
+    protected $signature = 'db:migrate-fresh';
 
     /**
      * The console command description.
