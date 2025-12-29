d349cb2f678c55705856c17f08171e72185b49ba
dafalagi <dafarizky34@gmail.com>
2025-12-29 19:21:34 +0700

    feat: add success messages for product operations

---
+++ lang/en/messages/success/product-feat-success-messages.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    /** Product */
+    'product' => [
+        'stored' => 'Product successfully stored.',
+        'updated' => 'Product successfully updated.',
+        'deleted' => 'Product successfully deleted.',
+        'fetched' => 'Product successfully fetched.',
+    ],
+];
