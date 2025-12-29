65c28c397017b6f75ae034100adcd45e65a47021
dafalagi <dafarizky34@gmail.com>
2025-12-29 16:53:14 +0700

    refactor: remove deprecated admin API routes and related files

--- routes/api/admin/file-system/file-system.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
-
-Route::prefix('file-system')->group(function() {
-    Route::post('', [\App\Http\Controllers\API\Admin\FileSystem\FileStorageController::class, 'store']);
-});
