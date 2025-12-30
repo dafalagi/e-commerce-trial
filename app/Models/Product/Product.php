e411a1cc8f6082e3955698acdbc02ba03f2b1a51
dafalagi <dafarizky34@gmail.com>
2025-12-30 14:35:16 +0700

    feat: add cartItems relationship and restrict on delete logic to Product model

--- app/Models/Product/Product.php
+++ app/Models/Product/Product.php
@@ -3,8 +3,21 @@
 namespace App\Models\Product;
 
 use App\Models\BaseModel;
+use App\Models\Order\CartItem;
 
 class Product extends BaseModel
 {
     protected $table = 'prd_products';
+
+    public function getRestrictOnDeleteRelations(): array
+    {
+        return [
+            'cartItems',
+        ];
+    }
+
+    public function cartItems()
+    {
+        return $this->hasMany(CartItem::class, 'product_id');
+    }
 }
